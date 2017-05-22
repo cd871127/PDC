b0301f9d93cf763b4ddbf4319b82a76208d2321d
Anthony <cdistc@163.com>
2017-05-22 14:58:58 +0800

    test

--- src/main/java/com/anthony/mvc/controller/summonerswar/SummonersWarController.java
+++ src/main/java/com/anthony/mvc/controller/summonerswar/SummonersWarController.java
@@ -2,15 +2,18 @@ package com.anthony.mvc.controller.summonerswar;
 
 import com.anthony.mvc.dto.summonerswar.MonsterFightTimeDTO;
 import com.anthony.mvc.util.summonerswar.FightNum;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by chend on 2017/4/16.
  */
-//@RestController
+@CrossOrigin(origins = "*")
+@RestController("/summoner")
 public class SummonersWarController {
-    @RequestMapping(value = "test")
+    @RequestMapping(value = "fighttime",method = RequestMethod.GET)
     public int calculateFightTimes(MonsterFightTimeDTO monsterFightTimeDTO) {
 //        monsterFightTimeDTO=new MonsterFightTimeDTO();
 //        monsterFightTimeDTO.setCurExp(0);
@@ -18,6 +21,7 @@ public class SummonersWarController {
 //        monsterFightTimeDTO.setStar(3);
 //        monsterFightTimeDTO.setFightExp(2552);
         int res = FightNum.getFightNum(monsterFightTimeDTO);
+        System.out.println(res);
         return res;
     }
 }
